c12efbc7502960d873a287a23c2ec99c25ef16df
Aleksander Lygren Toppe <aleksander@toppe.no>
2021-02-24 20:16:14 +0100

    Config configurable placement of menu button. Fixes #2.

--- MineralExhaustionNotifier/MineralExhaustionNotifier.cs
+++ MineralExhaustionNotifier/MineralExhaustionNotifier.cs
@@ -24,28 +24,37 @@ namespace DSPPlugins_ALT
         public static ConfigEntry<int> CheckPeriodSeconds;
         public static ConfigEntry<int> VeinAmountThreshold;
         public static ConfigEntry<bool> ShowMenuButton;
+        public static ConfigEntry<int> MenuButtonPlacementX;
+        public static ConfigEntry<int> MenuButtonPlacementY;
         public static ConfigEntry<KeyCode> ShowNotificationWindowHotKey;
-
+        
         public static MineralExhaustionNotifier instance;
 
         void InitConfig()
         {
             CheckPeriodSeconds = Config.Bind("General", "CheckPeriodSeconds", 5, "How often to check for miner problems");
             VeinAmountThreshold = Config.Bind("General", "VeinAmountThreshold", 6000, "Threshold of vein amount left to mine for adding the miner to the list");
-            ShowMenuButton = Config.Bind("General.Toggles", "ShowMenuButton", true, "Whether or not to show the menu button lower right");
-            ShowNotificationWindowHotKey = Config.Bind<KeyCode>("config", "ShowInformationWindowHotKey", KeyCode.I, "Key to press for toggling the Miner Information Window");
+            MenuButtonPlacementX = Config.Bind("General", "MenuButtonPlacementX", 45, "Menubutton X and Y Offset from bottom right of the screen. X,Y - 123, 46 = Under Tech; 45, 128 = Right under stat");
+            MenuButtonPlacementY = Config.Bind("General", "MenuButtonPlacementY", 128, "Menubutton X and Y Offset from bottom right of the screen. X,Y - 123, 46 = Under Tech; 45, 128 = Right under stat");
 
-            GUI.MinerNotificationUI.ShowButton = ShowMenuButton.Value;
+            ShowMenuButton = Config.Bind("General.Toggles", "ShowMenuButton", true, "Whether or not to show the menu button lower right");
+            ShowNotificationWindowHotKey = Config.Bind<KeyCode>("config", "ShowInformationWindowHotKey", KeyCode.I, "Key to press for toggling the Miner Information Window in addition to ALT");
+
+            // X,Y - 123, 46 = Under Tech; 45, 128 = Right under stat
         }
 
         #region Unity Core Methods
         // Awake is called once when both the game and the plug-in are loaded
         void Awake()
         {
-            instance = this;
-
-            minerNotificationUI = new MinerNotificationUI(minerStatistics);
             InitConfig();
+            instance = this;
+            minerNotificationUI = new MinerNotificationUI(minerStatistics);
+            GUI.MinerNotificationUI.ShowButton = ShowMenuButton.Value;
+            minerNotificationUI.MenuButtonXOffset = MenuButtonPlacementX.Value;
+            minerNotificationUI.MenuButtonYOffset = MenuButtonPlacementY.Value;
+
+
 
             UnityEngine.Debug.Log("Mineral Vein Exhaustion Plugin Loaded!");
             var harmony = new Harmony(VersionInfo.BEPINEX_FQDN_ID);
